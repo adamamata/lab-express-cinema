48e748ed13bb5ec4d2285112e4f14016d369aaa4
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-12 16:08:53 +0200

    completed iteration 3

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require('../models/Movie.model');
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+/* GET movies page */
+router.get('/movies', (req, res) => {
+    Movie.find()
+        .then((data) => {
+            res.render('movies', { data });
+        })
+        .catch((err) => console.log(err));
+});
 
 module.exports = router;
